1158ef5e2ca4099fca6425335769f634a17ca65c
Kosay Aboud <koabo22@student.sdu.dk>
2022-11-20 20:54:43 +0100

    few

--- 00000.py
+++ 00000.py
@@ -8,11 +8,12 @@ def compare (v:List[int],n:int)->tuple[int,int,int]:
     else:
         (gt,eq,lt)=compare (v[1:],n)
         if v[0]=>n:
-            return (gt+1,eq,lt)
+            return (gt+1,efewq,lt)
         elif v[0]==n:
-            return (gt,eq+1,lt)
+            return (gt,efewq+1,lt)
         else:
             return (gt,eq,lt+1)
         
         
             
+print (compare())
--- 4.py
+++ 4.py
@@ -3,5 +3,6 @@ name=input ("hvad hedder du? ")
 print ("hello"+ name )
 age= int("hvor gammel er du? ")
 print ( "du er"+ int(input) - int(2022))
+print
 
 
